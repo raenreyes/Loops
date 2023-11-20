32ff6e6cb7348dc5582dd23f82802e79956e12c8
Raen Reyes <raen.reyes11@outlook.com>
2023-11-19 23:40:17 -0800

    coding exercise 4 :loops

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -1,20 +1,33 @@
-﻿namespace Loops
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Loops
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter what grade you are in (1-4) in highschool");
-            string grade = Console.ReadLine();
-            int numberGrade = int.Parse(grade);
-            while (numberGrade < 1 || numberGrade > 4)
-            {
-                Console.WriteLine("Invalid please enter a valid grade number");
-                Console.WriteLine("Please enter what grade you are in (1-4) in highschool");
-                grade = Console.ReadLine();
-                numberGrade = int.Parse(grade);
-            }
-            Console.WriteLine("You entered a valid grade number of " + numberGrade);
+
+            ForLoop();
+            WhileLoop();
         }
+        public static void ForLoop() { 
+            for(int i = -3; i <= 3; i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+        }
+        public static void WhileLoop()
+        {
+            int num = 3;
+            while(num < 4 && num > -4)
+            {
+              
+                Console.WriteLine(num);
+                num--;
+
+            }
+        }
+        
     }
 }
