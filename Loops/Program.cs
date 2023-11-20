63a32d9b93ba2dbe6bcc94982d8b6e71f6cda5da
Raen Reyes <raen.reyes11@outlook.com>
2023-11-20 13:48:50 -0800

    My way of doing the challenge. Next will be the professor way of doing it

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,39 +6,61 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            Run();
+            Average();
+        
         }
+        public static void Average ()
+        {         
+            bool arraySuccess;
+            int counter = 0;
+            double average = 0;
+            int dontCount = 1;
+            do
+            {
+                Console.WriteLine("Enter how much test scores you will enter: ");
+                string arraySize = Console.ReadLine();
+                arraySuccess = int.TryParse(arraySize, out counter);
+            }
+            while (arraySuccess == false);
 
-        public static void Run()
-        {
-            int i = -10;
+            double[] inputs = new double[counter];
 
-            while (true) {
-                for (int counter = i; counter < 11; counter++)
+            for (int i = 0; i < counter; i++)
+            {                
+                bool success = false;
+                double parsedNum;
+                do
                 {
-                    if (counter % 3 == 0)
-                    {
-                        continue;
-                    }
-                    Console.WriteLine(counter);
-                }
-                if (i == 11)
+                    Console.WriteLine("Please score: " + dontCount);
+                    string numString = Console.ReadLine();
+                    success = double.TryParse(numString, out parsedNum);
+                    
+                    if (parsedNum < 0 || parsedNum > 20)
+                    {   
+                        if (parsedNum == -1)
+                        {
+                            break;
+                        }
+                        success = false;
+                    }                 
+                    inputs[i] = parsedNum;
+                } while (success == false);
+                dontCount++;
+                if (parsedNum == -1)
                 {
-                    Console.WriteLine(" FINAL BREAK REACHED! This should not happen!");
                     break;
                 }
-               
-                break;
+            }           
+            for (int j = 0; j < inputs.Length; j++)
+            {
+                 average += inputs[j];
+                
             }
+            Console.WriteLine("The total of all the points before dividing is " + average);
+            average = average / inputs.Length;
 
+            Console.WriteLine("The Average of all your numbers is " + average);
 
-                
-
-                
-               
-
-
-            
         }
     }
 }
